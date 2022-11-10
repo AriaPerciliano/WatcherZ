298aee5cfd1350e873441c4c5d2df6abc517c9d5
ariaNotFound404 <arianotfound404@gmail.com>
2022-11-10 03:29:40 -0300

    Update Seach.js

--- src/components/Menu/components/Seach.js
+++ src/components/Menu/components/Seach.js
@@ -17,15 +17,15 @@ const StyledSearch = styled.div`
     border: none;
     outline: none;
     color: ${({ theme }) => theme.textColorBase};
-    background-color: ${({ theme }) => theme.backgroundBase || "#F8EDFF"};
+    background-color: ${({ theme }) => theme.backgroundBase};
   }
   button {
     flex: 1;
     cursor: pointer;
     border: none;
-    background-color: ${({ theme }) => theme.backgroundLevel2 || "#D9C2EF"};
+    background-color: ${({ theme }) => theme.backgroundLevel2};
     box-shadow: 0 1px 0 rgb(0 0 0 / 10%);
-    border-left: 1px solid ${({ theme }) => theme.borderBase || "#E3D2F2"};
+    border-left: 1px solid ${({ theme }) => theme.borderBase};
     width: 40px;
     height: 40px;
     @media (min-width: 600px) {
