58f417046c1b2a336448d057cc8651ddb32dbfcd
ariaNotFound404 <arianotfound404@gmail.com>
2022-11-10 04:47:12 -0300

    Update DarkModeSwitch.js

--- src/components/Menu/components/DarkModeSwitch.js
+++ src/components/Menu/components/DarkModeSwitch.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import React from "react";
+import { ColorModeContext } from "./ColorMode";
 
 
 const StyledSwitch = styled.div`
@@ -36,10 +38,13 @@ const StyledSwitch = styled.div`
 `;
 
 export default function DarkModeSwitch() {
+  const contexto = React.useContext(ColorModeContext);
+
   return (
     <StyledSwitch>
       <input id="darkmode" type="checkbox" onChange={() => {
-        
+          console.log("mudou o state");
+          contexto.toggleMode();
       }} />
       <label
         htmlFor="darkmode"
