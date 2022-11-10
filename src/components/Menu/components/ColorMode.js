f088f114da36e0fa378cb60402519620843d5040
ariaNotFound404 <arianotfound404@gmail.com>
2022-11-10 04:47:08 -0300

    Update ColorMode.js

--- src/components/Menu/components/ColorMode.js
+++ src/components/Menu/components/ColorMode.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 export const ColorModeContext = React.createContext({
-    mode: "light"
+    mode: "",
+    setMode: () => ( alert("Configura-me ou devoro-te!")),
+    toggleMode: () => ( alert("Configura-me ou devoro-te!"))
 });
 
 export default function ColorModeProvider(props) {
+    const [mode, setMode] = React.useState(props.initialMode)
+
+    function toggleMode() {
+        if(mode === "light") setMode("dark")
+        if(mode === "dark") setMode("light")
+    }
+
     return (
-        <ColorModeContext.Provider>
+        <ColorModeContext.Provider value={{ mode: mode, setMode: setMode, toggleMode: toggleMode }}>
             {props.children}
         </ColorModeContext.Provider>
     )
