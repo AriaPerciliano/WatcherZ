b82e9c0f2b98e5d97349bc453efb1a56468fe453
ariaNotFound404 <arianotfound404@gmail.com>
2022-11-11 16:24:42 -0300

    Player de Video (WIP dms)

--- pages/video.js
+++ pages/video.js
@@ -1,47 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import VideoPlayer from "../src/components/VideoPlayer";
 import Menu from "../src/components/Menu";
-import { ColorModeContext } from "../src/components/Menu/components/ColorMode";
 
-
-export default function Video() {
-    const contexto = React.useContext(ColorModeContext);
-    return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            flex: 1,
-        }}>
-            <Menu />
-            <PlayerVideo />
-        </div>
-    )
-}
-const StyledVideoWindow = styled.div`
-    .video-window {
-        display: flex;
-        align-items: center;
-        height: 100%;
-        width: 60%;
-        padding: 16px 16px 26px;
-        gap: 16px;
-        font-size: 20px;
-        background-color: ${({ theme }) => theme.backgroundLevel1};
-        border-radius: 0px 0px 10px 10px;
-    }
-    iframe {
-        padding: 50px;
-        height: 720px;
-        width: 1280px;
-    }
+const VideoPlayer = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
 `;
 
-function PlayerVideo() {
-    return(
-        <StyledVideoWindow>
-            <section className="video-window">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ddHa1fCB4Gg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </section>
-        </StyledVideoWindow>
+const Video = () => {
+    const [searchValue, setSearchValue] = useState("");
+
+    return (
+        <StyledVideo>
+            <Menu searchValue={searchValue} setSearchValue={setSearchValue} />
+            <VideoPlayer />
+        </StyledVideo>
     )
 }
+
+export default Video;
---
+++ src/components/VideoPlayer/index.js
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import styled from "styled-components";
+
+const StyledVideoPlayer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    .title {
+        padding: 16px;
+    }
+`;
+
+const VideoPlayer = () => {
+    const router = useRouter();
+    return (
+        <StyledVideoPlayer>
+            <h2 className="title">{router.query.title}</h2>
+            <iframe 
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${router.query.v}`}
+            title="Video Player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            ></iframe>
+        </StyledVideoPlayer>
+    )
+}
+
+export default VideoPlayer;
